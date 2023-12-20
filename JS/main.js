595d249be74f0a0189a122efe6f23cd4f530d72c
LunaABath <lunaagustinabath@gmail.com>
2023-12-20 15:48:39 -0300

    I work on category sections and html reports, and add some utilities to js file.

--- JS/main.js
+++ JS/main.js
@@ -3,3 +3,36 @@
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
 
+const randomId = () => self.crypto.randomUUID()
+
+const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+const getData = (key) => JSON.parse(localStorage.getItem(key));
+
+const hiddenElement = (selectors) => {
+    for (const selector of selectors) {
+        $(selector).classList.add("hidden")
+    }
+}
+
+/* Nav events */
+$("#balance-btn-nav").addEventListener('click', () => {
+    $("#balances-section").classList.remove('hidden');
+    hiddenElement(["#new-operations-section", "#category-section", "#reports-section", "#category-edit-section"])
+});
+
+
+$("#category-btn-nav").addEventListener('click', () => {
+    $("#category-section").classList.remove('hidden');
+    hiddenElement(["#balances-section", "#new-operations-section", "#reports-section", "#category-edit-section"])
+});
+
+$("#reports-btn-nav").addEventListener('click', () => {
+    $("#reports-section").classList.remove('hidden');
+    hiddenElement(["#category-section", "#balances-section", "#new-operations-section", "#category-edit-section"])
+});
+
+
+$("#new-operation-btn").addEventListener('click', () => {
+    $("#new-operations-section").classList.remove('hidden');
+    hiddenElement(["#reports-section", "#category-section", "#balances-section", "#category-edit-section"]);
+})
